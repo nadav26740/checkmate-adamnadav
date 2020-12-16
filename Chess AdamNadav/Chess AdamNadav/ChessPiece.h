e272293d340e484f388e853d384e736e4247b3ec
Nadav Mordechai <nadav26740@gmail.com>
2020-12-16 16:26:25 +0200

    ChessPiece Header v1.0 - Nadav .M.

--- Chess AdamNadav/Chess AdamNadav/ChessPiece.h
+++ Chess AdamNadav/Chess AdamNadav/ChessPiece.h
@@ -1,5 +1,14 @@
 #pragma once
 class ChessPiece
 {
+private:
+	int  _cords[2]; // { x, y }
+	bool _white;
+
+public:
+	int* getlocation(); // return _cords
+	virtual void move(int* newCords, bool playerWhite) = 0; // virtual
+	bool getWhite(); // return _White
+
 };
 
