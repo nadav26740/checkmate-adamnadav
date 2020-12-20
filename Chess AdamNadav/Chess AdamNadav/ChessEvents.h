78c284d3a5e0b658ca4efe2ddc031c9ea4e000cd
Nadav Mordechai <nadav26740@gmail.com>
2020-12-20 13:05:29 +0200

    Chess Events added - nadav .M.

---
+++ Chess AdamNadav/Chess AdamNadav/ChessEvents.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+enum { // events Defines
+	VALIED_MOVE = 0,
+	VALIED_CHESS, // 1
+	INVALIED_PIECE_NOT_ALLOWED, // 2
+	INVALIED_DEST_PIECE_OWN, // 3
+	INVALIED_CHESS_WILL_BECOME, // 4
+	INVALIED_OUT_OF_INDEX, // 5
+	INVALIED_PIECE_MOVE, // 6
+	INVALIED_SOURCE_EQL_DEST, // 7
+	VALIED_CHESS_MATE // 8
+};
+
+class ChessEvents
+{
+};
+
