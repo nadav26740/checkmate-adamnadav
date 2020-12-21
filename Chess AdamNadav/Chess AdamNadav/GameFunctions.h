aa814b864b45e86433885afc871d9ea2881df572
Nadav Mordechai <nadav26740@gmail.com>
2020-12-21 16:11:05 +0200

    GameFunction Fixed!

--- Chess AdamNadav/Chess AdamNadav/GameFunctions.h
+++ Chess AdamNadav/Chess AdamNadav/GameFunctions.h
@@ -3,9 +3,10 @@
 #include "King.h"
 #include "Bishop.h"
 #include "King.h"
+
 class GameFunctions
 {
 public:
-	static ChessPiece* createPieceChar(char type); // getting char and creating Dynamic ChessPiece	
+	static ChessPiece* createPieceChar(char type); // getting char and creating Dynamic ChessPiece
 };
 
